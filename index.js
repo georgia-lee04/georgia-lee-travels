4c8dde37a9cffdc1c2c9ff94f347e7b260f2db39
georgia-lee04 <xgeorgialee@gmail.com>
2025-04-09 20:22:46 +0100

    created new file for swiper

--- index.js
+++ index.js
@@ -26,39 +26,3 @@ function generateTripIdeas(event) {
 
 let tripIdeasForm = document.querySelector("#trip-ideas-form");
 tripIdeasForm.addEventListener("submit", generateTripIdeas);
-
-const swiper = new Swiper(".wrapper", {
-  loop: true,
-  spaceBetween: 30,
-
-  autoplay: {
-    delay: 5000,
-    disableOnInteraction: false,
-    pauseOnMouseEnter: true,
-  },
-
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    dynamicBullets: true,
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    620: {
-      slidesPerView: 2,
-    },
-    1024: {
-      slidesPerView: 3,
-    },
-  },
-});
